770bb7ff1d3d2a8c426b46a80fd05e92d88d9535
Iñaki Ulibarri Utrilla <i.ulibarri.utrilla@gmail.com>
2024-02-04 17:19:14 -0600

    made performance enhancements for makeCards() and added more cards for testing

--- src/WarhammerCards.jl
+++ src/WarhammerCards.jl
@@ -1,4 +1,3 @@
-# TODO: optimise makeCards()
 # TODO: add more cards for printing
 # TODO: be able to make cards for a single faction/
 
@@ -29,7 +28,8 @@ function makeCard(card :: Card, r)
     squircle(O, bgw/2, bgh/2, action = :fill, rt = r)
     squircle(O, bgw/2, bgh/2, action = :clip, rt = r)
     imgpath = pkgdir(WarhammerCards, "data", "images", card.bimage)
-    img = readpng(imgpath) 
+    img = readpng(imgpath)
+    # scale(img.width/bgw, img.height/bgh)
     placeimage(img, O, 0.2, centered = true)
     # title
     setcolor("grey7")
@@ -53,9 +53,44 @@ end
 function makeCards(fname :: String)
     cards = read(fname, String)
     cards = JSON3.read(cards)
+    w, h = 600, 800
+    r = 0.05
+    imgpath = ""
+    img = nothing
     for c ∈ cards
         card = cardOfDict(c)
-        makeCard(card, 0.05)
+        Drawing(w, h, card.title * ".png")
+        origin()
+        # chanfers
+        setcolor(card.scolor) 
+        squircle(O, w/2, h/2, action = :fill, rt = r)
+        # background
+        bgw, bgh = w-100, h-100
+        setcolor(card.pcolor)
+        squircle(O, bgw/2, bgh/2, action = :fill, rt = r)
+        squircle(O, bgw/2, bgh/2, action = :clip, rt = r)
+        ppath = pkgdir(WarhammerCards, "data", "images", card.bimage) # possible path
+        if imgpath != ppath
+            # don't load the same image twice, 
+            # just if it has to be changed
+            imgpath = ppath
+            img = readpng(imgpath)
+            # scale(img.width/bgw, img.height/bgh)        
+        end
+        placeimage(img, O, 0.2, centered = true)
+        # title
+        setcolor("grey7")
+        fontface("Vollkorn")
+        tbw, tbh = bgw, 180
+        tbox = BoundingBox(box(Point(0, (-bgh + tbh)/2), tbw, tbh))
+        textfit(card.title, tbox; horizontalmargin = 12)
+        setline(6)
+        line(Point(-w/2+50, -h/2+50+tbh), Point(w/2-50, -h/2+50+tbh), action = :stroke)
+        # body of the card
+        bbw, bbh = bgw, bgh - tbh
+        bbox = BoundingBox(box(Point(0, (bgh - bbh)/2), bbw, bbh))
+        textfit(card.body, bbox, 52; horizontalmargin = 12)
+        finish()
     end
 end
 
--- test/runtests.jl
+++ test/runtests.jl
@@ -1,10 +1,5 @@
 using WarhammerCards
 
-# c = Card("Rune of Wrath & Ruin", "Die!",
-#          "burlywood2" ,"dodgerblue4",
-#          "Dwarfhead-0.png"))
-
-# makeCard(c, 0.05)
-
 makeCards(pkgdir(WarhammerCards, "data", "cardInfo", "dwarfs", "dwarfs.json"))
+makeCards(pkgdir(WarhammerCards, "data", "cardInfo", "daemons", "daemons.json"))
 
